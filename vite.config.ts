8293b5cdd18b3b0dac81590495b01037f7abf94c
MapIhs <maptuhul2011@gmail.com>
2025-12-02 06:23:27 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 // vite.config.ts
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [enhancedImages(), sveltekit(), tailwindcss()]
 });
